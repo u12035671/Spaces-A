cf8321f9331dda3c50be515977d427d9fffbed6b
Mpedi Mello <mpedi@mpedi-Aspire-E1-571.(none)>
2015-03-20 15:06:33 +0200

    added closebuzzSpace

--- GroupA_Discussion_Board/modules/Spaces/spaces.js
+++ GroupA_Discussion_Board/modules/Spaces/spaces.js
@@ -4,8 +4,26 @@
 /*
     This module will be used to create and close various Buzz Spaces
  */
-exports.closeBuzzSpace = function () {
-    return "Space closed";
+ function isAuthorized(userId, moduleId) { return true; };
+ 
+exports.closeBuzzSpace = function (userId, moduleId) {
+	var obj = {};
+	// check if space exists
+	db.find({'moduleId':moduleId}, function (err, result) {
+		if (err) {
+			throw new Error("NoSuchBuzzSpaceException");
+		} else {
+			// check if user is an admin for buzz space
+			if (isAuthorized(userId, moduleId)) {
+				result.isOpen = false;
+			} else {
+				throw new Error("NotAuthorizedException");
+			}
+			obj = result;
+		}
+	});
+	
+	return obj;
 };
 
 exports.createBuzzSpace = function () {
