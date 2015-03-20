6467889c174085684b4ec3f298b7ab290eb72f6a
gelid <zander.boshoff@gmail.com>
2015-03-20 10:10:47 +0200

    Added createBuzzSpace
    I'm not sure if it's right or how to test it

--- GroupA_Discussion_Board/modules/Spaces/spaces.js
+++ GroupA_Discussion_Board/modules/Spaces/spaces.js
@@ -8,6 +8,22 @@ exports.closeBuzzSpace = function () {
     return "Space closed";
 };
 
-exports.createBuzzSpace = function () {
-    return "Space created";
+exports.createBuzzSpace = function (academicYear,isOpen,moduleID,name,adminUsers) {
+
+    var sp = require('../Database/connect.js');
+    var newBuzzSpace = {};
+    //check if the module exists
+    db.find({'moduleID':moduleID},function (err,result) {
+        if (err) {
+            newBuzzSpace = {'academicYear':academicYear,'isOpen':isOpen,'moduleID':moduleID,'name':name,'adminUsers':adminUsers};
+            var collection = db.collection('Spaces');
+            collection.insert(newBuzzSpace);
+        }
+        else{
+            newBuzzSpace = result;
+        }
+
+    })
+
+    return newBuzzSpace;
 };
