b26f9539b3125cc1fffda9597770564b1423031a
MattNel <matt.nel@hotmail.com>
2015-04-06 21:02:35 +0200

    created interface for createSpace, closeSpace and adminManagement

--- v 1.0/cos301_Phase3/Handlebars Server/routes/index.js
+++ v 1.0/cos301_Phase3/Handlebars Server/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyparser = require('body-parser');
+var urlencodedparser = bodyparser.urlencoded({extended:false});
 var router = express.Router();
+
+var spaces = require('../modules/spaces/spaces');
 
 function getProfile (id) {
  return {title: "user " + id};
@@ -21,6 +25,77 @@ router.get('/testing', function(req, res, next) {
   res.render('test', getProfile(req.query.id));
 });
 
+//create space
+router.get('/createSpace', function(req, res, next) {
+//Pass to page
+  res.render('./dynamic_views/createSpace');
+});
 
+//Close space
+router.get('/closeSpace', function(req, res, next) {
+//Pass to page
+  res.render('./dynamic_views/closeSpace');
+});
+
+
+//Admin management
+router.get('/adminManagement', function(req, res, next) {
+//Pass to page
+  res.render('./dynamic_views/adminManagement');
+});
+
+router.post('/submitCS', function(req, res, next){
+	/*
+	console.log("test ["+req.body.CSyear+"]");
+	console.log("test ["+req.body.CSmodule+"]");
+	console.log("test ["+req.body.CSname+"]");
+	*/
+	var obj ={};
+		obj.academicYear=req.body.CSyear;
+		obj.isOpen = true;
+		obj.moduleID = req.body.CSmodule;
+		obj.name = req.body.CSname;
+		obj.adminUsers = [];
+		
+	var result = spaces.createBuzzSpace(obj);
+	
+	console.log("this is the result " + result);
+	res.render('./dynamic_views/createSpace',{message: result});
+});
+
+router.post('/submitRS', function(req, res, next){
+	//console.log("test ["+req.body.RSmodule+"]");
+	//console.log("test ["+req.body.RSid+"]");
+	var obj = {};
+		obj.moduleID = req.body.RSmodule;
+		obj.userID = req.body.RSid;
+		
+	var result = spaces.closeBuzzSpace(obj);
+	
+	res.render('./dynamic_views/closeSpace',{message: result});
+});
+
+router.post('/submitAAM', function(req, res, next){
+	//console.log("test ["+req.body.Aadmin+"]");
+	//console.log("test ["+req.body.AAid+"]");
+	var obj = {};
+		obj.moduleID = req.body.Aadmin;
+		obj.userID = req.body.AAid;
+		
+	var result = spaces.assignAdministrator(obj);
+	
+	res.render('./dynamic_views/adminManagement',{message: result});
+});
+
+router.post('/submitRAM', function(req, res, next){
+	//console.log("test ["+req.body.Radmin+"]");
+	//console.log("test ["+req.body.AAid+"]");
+	var obj = {};
+		obj.moduleID = req.body.Radmin;
+		obj.userID = req.body.RAid;
+		
+	var result = spaces.removeAdministrator (obj);
+	res.render('./dynamic_views/adminManagement',{message: result});
+});
 
 module.exports = router;
