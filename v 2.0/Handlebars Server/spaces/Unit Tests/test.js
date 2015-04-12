ac0e7872ae359bcf46a984f65b83317d1131a254
shaunmeintjes <shaunmeintjes@hotmail.com>
2015-04-12 18:38:04 +0200

    Added to Unit Tests

--- v 2.0/Handlebars Server/spaces/Unit Tests/test.js
+++ v 2.0/Handlebars Server/spaces/Unit Tests/test.js
@@ -1,21 +1,18 @@
 var spaces = require('./buzz-spaces/spaces.js');
-
-//console.log(spaces.createBuzzSpace(2015,true,'zzz','Jan',[{id:1},{id:2}]));
-//console.log(spaces.closeBuzzSpace('1','aaa'));
-//console.log(spaces.assignAdministrator('2015', true, 'zzz', 'Jan', [{id:1},{id:2}], {id:3}, '1'));
-//console.log(spaces.removeAdministrator('1', 'zzz', {id:2}));
-
-//try to register on buzz space before space exists
-
-temp.registerOnBuzzSpace
-
-var createBuzzObj ={};
-		createBuzzObj.userNameForBuzzSpace=req.body.RUuserName;
-		createBuzzObj.signature=req.body.RUsignature;
-		createBuzzObj.userID=req.body.RUuserid;
-		createBuzzObj.moduleID=req.body.RUmoduleid;
-		
-	var result = spaces.createBuzzSpace(createBuzzObj);
 	
+	var assignAdminObj = {};
+		assignAdminObj.moduleID='zzz';
+		assignAdminObj.userID='1';
+		assignAdminObj.newAdmin={id:3};
+		assignAdminObj.adminUsers=[{id:1},{id:2}];
+		
+	var result = spaces.assignAdministrator(assignAdminObj);   
+	
+	var removeAdminObj = {};
+		removeAdminObj.moduleID='zzz';
+		removeAdminObj.userID='1';
+		removeAdminObj.adminToRemove={id:3};
+		
+	var result = spaces.assignAdministrator(removeAdminObj);   
 	
 //Ivan please add your 2 functions below this comment, I am busy with mine above this line
