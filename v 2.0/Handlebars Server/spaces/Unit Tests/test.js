b3b912bcb3f314938cd7625a724453a9e2b17473
ivanhenning <ivanace87@gmail.com>
2015-04-14 20:25:43 +0200

    test for create and close

--- v 2.0/Handlebars Server/spaces/Unit Tests/test.js
+++ v 2.0/Handlebars Server/spaces/Unit Tests/test.js
@@ -35,3 +35,41 @@ var spaces = require('./buzz-spaces/spaces.js');
 	var result = spaces.registerOnBuzzSpace(registerObj);   
 	
 //Ivan please add your 2 functions below this comment, I am busy with mine above this line
+
+function testCreateAndClose()
+{
+	var createSpaceTest1 = {};
+		createSpaceTest1.academicYear='2';
+        createSpaceTest1.isOpen='yes';
+        createSpaceTest1.moduleID='1';
+        createSpaceTest1.name'test';
+        createSpaceTest1.adminUsers=0;
+		
+	var createSpaceTest2 = {};
+		createSpaceTest2.academicYear='2';
+        createSpaceTest2.isOpen='yes';
+        createSpaceTest2.moduleID='1';
+        createSpaceTest2.name'test';
+        createSpaceTest2.adminUsers=0;
+		
+	var createSpaceTest3 = {};
+		createSpaceTest3.academicYear='1';
+        createSpaceTest3.isOpen='yes';
+        createSpaceTest3.moduleID='2';
+        createSpaceTest3.name'test2';
+        createSpaceTest3.adminUsers=0;
+	
+	//Create two new spaces to test
+	var testCreate1 = spaces.createBuzzSpace(createSpaceTest1);
+	var testCreate2 = spaces.createBuzzSpace(createSpaceTest2);
+	
+	//Delete one of the earlier created spaces to test
+	var testClose1 = spaces.closeBuzzSpace(createSpaceTest2);
+	
+	//Create the deleted space once again
+	var testCreate3 = spaces.createBuzzSpace(createSpaceTest2);
+	
+	//Delete a non existing space
+	var testClose2 = spaces.closeBuzzSpace(createSpaceTest3);
+	
+}
