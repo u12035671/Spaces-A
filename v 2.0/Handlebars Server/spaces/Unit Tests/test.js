64fcec8a07fa44b661a806c957b37c352c2788f4
shaunmeintjes <shaunmeintjes@hotmail.com>
2015-04-15 22:50:17 +0200

    Added functions to Unit Tests

--- v 2.0/Handlebars Server/spaces/Unit Tests/test.js
+++ v 2.0/Handlebars Server/spaces/Unit Tests/test.js
@@ -1,38 +1,52 @@
 var spaces = require('./buzz-spaces/spaces.js');
 	
-	var assignAdminObj = {};
-		assignAdminObj.moduleID='zzz';
-		assignAdminObj.userID='1';
-		assignAdminObj.newAdmin={id:3};
-		assignAdminObj.adminUsers=[{id:1},{id:2}];
+	function testAssignAdmin()
+	{
+		var assignAdminObj = {};
+			assignAdminObj.moduleID='zzz';
+			assignAdminObj.userID='1';
+			assignAdminObj.newAdmin={id:3};
+			assignAdminObj.adminUsers=[{id:1},{id:2}];
 		
-	var result = spaces.assignAdministrator(assignAdminObj);   
+		var resultAssignAdmin = spaces.assignAdministrator(assignAdminObj);   
+	}
 	
-	var removeAdminObj = {};
-		removeAdminObj.moduleID='zzz';
-		removeAdminObj.userID='1';
-		removeAdminObj.adminToRemove={id:3};
-		
-	var result = spaces.removeAdministrator(removeAdminObj);   
+	function testRemoveAdmin()
+		var removeAdminObj = {};
+			removeAdminObj.moduleID='zzz';
+			removeAdminObj.userID='1';
+			removeAdminObj.adminToRemove={id:3};
+			
+		var resultRemoveAdmin = spaces.removeAdministrator(removeAdminObj);   
+	}
 	
-	var isAdminObj = {};
-		isAdminObj.moduleID='zzz';
-		isAdminObj.userID='1';
-		
-	var result = spaces.isAdministrator(isAdminObj);  
+	function testIsAdmin()
+	{
+		var isAdminObj = {};
+			isAdminObj.moduleID='zzz';
+			isAdminObj.userID='1';
+			
+		var resultIsAdmin = spaces.isAdministrator(isAdminObj);  
+	}
 	
-	var getProfileObj = {};
-		getProfileObj.userID='1';
-		
-	var result = spaces.getProfileForUser(getProfileObj); 
+	function testGetProfile()
+	{
+		var getProfileObj = {};
+			getProfileObj.userID='1';
+			
+		var resultGetProfile = spaces.getProfileForUser(getProfileObj); 
+	}
 	
-	var registerObj = {};
-		registerObj.moduleID='zzz';
-		registerObj.userID='1';
-		registerObj.userNameForBuzzSpace=//username//;
-		registerObj.signature=//signature//;
-		
-	var result = spaces.registerOnBuzzSpace(registerObj);   
+	function testRegisterOnBuzz()
+	{
+		var registerObj = {};
+			registerObj.moduleID='zzz';
+			registerObj.userID='1';
+			registerObj.userNameForBuzzSpace=//username//;
+			registerObj.signature=//signature//;
+			
+		var resultRegisterOnBuzz = spaces.registerOnBuzzSpace(registerObj);   
+	}
 	
 //Ivan please add your 2 functions below this comment, I am busy with mine above this line
 
